0bc5172574e63f46ed552eae7c696968383fe214
정용훈 <ka7713@naver.com>
2021-03-18 14:38:20 +0900

    mo2 plugin

---
+++ mo2_plugin/game_koikatsu.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+
+from typing import Optional, List
+
+import mobase
+
+from ..basic_game import BasicGame
+
+class KoikatsuGame(BasicGame):
+
+    Name = "Koikatsu Support Plugin"
+    Author = "Goodcat"
+    Version = "0.5.0"
+
+    GameName = "Koikatsu"
+    GameShortName = "Koikatsu"
+    GameBinary = "Koikatu.exe"
+    GameDataPath = ""
